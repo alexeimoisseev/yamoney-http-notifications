8f6ee12e0b9858b40fbec718ee754f31efadae70
Alexei Moisseev <alexmois@ya.ru>
2014-01-04 11:14:35 +0400

    first commit

---
+++ yamoney-http-notifications.js
@@ -0,0 +1,26 @@
+var crypto = require('crypto');
+var shasum = crypto.createHash('sha1');
+
+exports.index = function(secret, callback) {
+    return function(req, res) {
+        var checkString = req.body.notification_type + '&' +
+            req.body.operation_id + '&' +
+            req.body.amount + '&' +
+            req.body.currency + '&' +
+            req.body.datetime + '&' +
+            req.body.sender + '&' +
+            req.body.codepro + '&' +
+            secret + '&' +
+            req.body.label;
+        shasum.update(checkString);
+        var mySha = shasum.digest('hex');
+        if (mySha == req.body.sha1_hash) {
+            console.log('success for', req.body.operation_id);
+            res.send(200);
+            callback(null, req.body);
+        } else {
+            callback("SHA1 hash check failed");
+            res.send(400, 'Checksum failed');
+        }
+    }
+};
